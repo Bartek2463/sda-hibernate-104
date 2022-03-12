7ef6229449170214dde07ab0adb4ab2436d568a0
Michal Paukszto <michal.paukszto@gmail.com>
2022-03-12 12:13:21 +0100

    inital commit

---
+++ src/main/java/com/sda/hibernate/Main1Preparation.java
@@ -0,0 +1,14 @@
+package com.sda.hibernate;
+
+import org.hibernate.SessionFactory;
+
+public class Main1Preparation {
+
+    // MetadataSources metadataSources = new MetadataSources(registry);
+    // Metadata metadata = metadataSources.buildMetadata();
+    // SessionFactory sessionFactory = metadata.buildSessionFactory();
+
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtils.getSessionFactory();
+    }
+}
